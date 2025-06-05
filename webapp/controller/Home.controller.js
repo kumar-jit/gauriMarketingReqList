2e5fff69f6bf71f9712ac9d040d0c492bc5b715a
christo-p-joy <christo@maventic.com>
2025-06-05 15:47:22 +0530

    Filters, navigation

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,30 +10,65 @@ sap.ui.define([
             },
             onBeforeRebind(oEvent) {
 
-                var oBindingParams = oEvent.getParameter("bindingParams");
-
+                const oBindingParams = oEvent.getParameter("bindingParams");
+                const oModel = this.getView().getModel("filterModel");
+                const oData = oModel.getData();
+                const aData = Object.entries(oData);
+                aData.forEach(element => {
+                    if(element[1])
+                        switch (element[0]) {
+                            case "Status":
+                                oBindingParams.filters.push(
+                                    new sap.ui.model.Filter("Status", sap.ui.model.FilterOperator.Contains,element[1]),
+                                )
+                                break;
+                            case "RequesterId":
+                                oBindingParams.filters.push(
+                                    new sap.ui.model.Filter("RequesterId", sap.ui.model.FilterOperator.Contains,element[1]),
+                                )
+                            
+                                break;
+                            case "RequestId":
+                                oBindingParams.filters.push(
+                                    new sap.ui.model.Filter("RequestId", sap.ui.model.FilterOperator.Contains,element[1]),
+                                )
+                                break;
+                            case "InitiatedDate":
+                                oBindingParams.filters.push(
+                                    new sap.ui.model.Filter("InitiatedDate", sap.ui.model.FilterOperator.Contains,element[1]),
+                                )
+                                break;
+                            case "PermitType":
+                                oBindingParams.filters.push(
+                                    new sap.ui.model.Filter("PermitType", sap.ui.model.FilterOperator.Contains,element[1]),
+                                )
+                                break;
+                            default:
+                                break;
+                        }
+                });
                 // Clear existing filters if needed
-                oBindingParams.filters = [];
-                var sCustomValue = this.byId("idDatePicker")?.getValue();
-                if (sCustomValue) {
-                    oBindingParams.filters.push(
-                        new sap.ui.model.Filter("InitiatedDate", sap.ui.model.FilterOperator.LT, sCustomValue),
-                    )
-                }
+               
 
             },
-            onSearch(oEvent) {
-                var oTable = this.byId("_IDGenSmartTable");
+            onSelectionChange(oEvent) {
+                const oModel = this.getView().getModel("filterModel");
+                const combTxt = oEvent.getSource().getSelectedItem().getProperty("text");
+                oModel.setProperty("/PermitType", combTxt);
+
             },
-            onPressNavigate(oEvent) {
-                letCrossApplicationNavigation = sap.ushell.Container.getService("CrossApplicationNavigation");
-                CrossApplicationNavigation.toExternal({
+
+            onColumnListItemPress(oEvent) {
+                const sPath = oEvent.getSource().getBindingContext().getPath()
+                const reqId = sPath.match(/\d+/)[0];
+                let CrossApplicationNavigation = sap.ushell.Container.getService("CrossApplicationNavigation");
+                CrossApplicationNavigation.hrefForExternal({
                     target: {
                         semanticObject: "ZMKTCAMP",
                         action: "display"
                     },
                     params: {
-                        "RequestId": "102343333"
+                        "RequestId": reqId
                     }
                 }).then(function (sHref) {
                     // Place sHref somewhere in the DOM
