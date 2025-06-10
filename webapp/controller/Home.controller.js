1d2707dc4178587b858af303ce951f99a793275d
christo-p-joy <christo@maventic.com>
2025-06-10 22:59:33 +0530

    change in date filter

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -15,67 +15,78 @@ sap.ui.define([
                 const oData = oModel.getData();
                 const aData = Object.entries(oData);
                 aData.forEach(element => {
-                    if(element[1])
+                    if (element[1])
                         switch (element[0]) {
                             case "Status":
                                 oBindingParams.filters.push(
-                                    new sap.ui.model.Filter("Status", sap.ui.model.FilterOperator.Contains,element[1]),
+                                    new sap.ui.model.Filter("Status", sap.ui.model.FilterOperator.Contains, element[1]),
                                 )
                                 break;
                             case "RequesterId":
                                 oBindingParams.filters.push(
-                                    new sap.ui.model.Filter("RequesterId", sap.ui.model.FilterOperator.Contains,element[1]),
+                                    new sap.ui.model.Filter("RequesterId", sap.ui.model.FilterOperator.Contains, element[1]),
                                 )
 
                                 break;
                             case "RequestId":
                                 oBindingParams.filters.push(
-                                    new sap.ui.model.Filter("RequestId", sap.ui.model.FilterOperator.Contains,element[1]),
+                                    new sap.ui.model.Filter("RequestId", sap.ui.model.FilterOperator.Contains, element[1]),
                                 )
                                 break;
                             case "InitiatedDate":
+                                var date = element[1].split(" - ")
                                 oBindingParams.filters.push(
-                                    new sap.ui.model.Filter("InitiatedDate", sap.ui.model.FilterOperator.Contains,element[1]),
+                                    new sap.ui.model.Filter({
+                                        filters: [
+                                            new sap.ui.model.Filter("InitiatedDate", sap.ui.model.FilterOperator.GE, date[0]),
+                                            new sap.ui.model.Filter("InitiatedDate", sap.ui.model.FilterOperator.LE, date[1])
+                                        ],
+                                        and: true
+                                    })
                                 )
-                                break;
+
+                break;
                             case "PermitType":
-                                oBindingParams.filters.push(
-                                    new sap.ui.model.Filter("PermitType", sap.ui.model.FilterOperator.Contains,element[1]),
-                                )
+            oBindingParams.filters.push(
+                new sap.ui.model.Filter("PermitType", sap.ui.model.FilterOperator.Contains, element[1]),
+            )
                                 break;
-                            default:
+            default:
                                 break;
-                        }
+        }
                 });
                 // Clear existing filters if needed
-               
+
 
             },
-            onSelectionChange(oEvent) {
-                const oModel = this.getView().getModel("filterModel");
-              let combTxt = oEvent.getSource()?.getSelectedItem()?.getProperty("text");
-                if(combTxt)
-                    oModel.setProperty("/PermitType", combTxt);
-                else{
-                    oModel.setProperty("/PermitType", "");
-                }
-            },
 
-            onColumnListItemPress(oEvent) {
-                const sPath = oEvent.getSource().getBindingContext().getPath()
-                const reqId = sPath.match(/\d+/)[0];
-                let CrossApplicationNavigation = sap.ushell.Container.getService("CrossApplicationNavigation");
-                CrossApplicationNavigation.toExternal({
-                    target: {
-                        semanticObject: "ZMKTCAMP",
-                        action: "display"
-                    },
-                    params: {
-                        "RequestId": reqId
-                    }
-                }).then(function (sHref) {
-                    // Place sHref somewhere in the DOM
-                });
-            }
+_setComboBoxText(oEvent, propertyName) {
+    const oModel = this.getView().getModel("filterModel");
+    const combTxt = oEvent.getSource().getSelectedItem()?.getProperty("text") || "";
+    oModel.setProperty("/" + propertyName + "", combTxt);
+},
+onSelectionChange(oEvent) {
+    this._setComboBoxText(oEvent, "PermitType");
+},
+onStatusChange(oEvent) {
+    this._setComboBoxText(oEvent, "Status");
+},
+
+onColumnListItemPress(oEvent) {
+    const sPath = oEvent.getSource().getBindingContext().getPath()
+    const reqId = sPath.match(/\d+/)[0];
+    let CrossApplicationNavigation = sap.ushell.Container.getService("CrossApplicationNavigation");
+    CrossApplicationNavigation.toExternal({
+        target: {
+            semanticObject: "ZMKTCAMP",
+            action: "display"
+        },
+        params: {
+            "RequestId": reqId
+        }
+    }).then(function (sHref) {
+        // Place sHref somewhere in the DOM
+    });
+}
         });
     });
